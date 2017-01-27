0f9d89c58acf9029dde6d6b4b5a4abde77c5f766
stackoverflow2 <rameshwardhondge@gmail.com>
2017-01-28 01:08:55 +0530

    scrollspy

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  $('body').scrollspy({target: ".navbar", offset: 50});   
+  $("#myNavbar a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
